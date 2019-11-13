4b3d6bf03cfa8ddbb04d4e35b348d91e4556b686
Sascha Müllner <sascha.muellner@miele.com>
2019-11-13 17:20:09 +0100

    added geoip

--- legal.sh
+++ legal.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+echo "== ifconfig =="
 ifconfig
+
+echo "== GEO IP =="
+curl -s https://ipvigilante.com/$(curl -s https://ipinfo.io/ip)
+
+echo "== TRACEROUTE =="
+traceroute m.miele.com
 
 echo "curl -I https://m.miele.com/apps4/www-services/app-legal.aspx?lang=de&type=app-legal"
 curl -I https://m.miele.com/apps4/www-services/app-legal.aspx?lang=de&type=app-legal
