ba8f04279cfb435a092e8c739218b627ae795a83
Sascha Müllner <sascha.muellner@miele.com>
2019-11-13 17:28:02 +0100

    Added a redirection check

--- legal.sh
+++ legal.sh
@@ -10,8 +10,16 @@ curl -s https://ipvigilante.com/$(curl -s https://ipinfo.io/ip)
 echo "== TRACEROUTE =="
 traceroute m.miele.com
 
+echo "== NOT REDIRECTED =="
+
 echo "curl -I https://m.miele.com/apps4/www-services/app-legal.aspx?lang=de&type=app-legal"
 curl -I https://m.miele.com/apps4/www-services/app-legal.aspx?lang=de&type=app-legal
 
 echo "curl -I https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal"
 curl -I https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal
+
+
+echo "== REDIRECTED =="
+
+echo "curl -I -L https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal"
+curl -I -L https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal
