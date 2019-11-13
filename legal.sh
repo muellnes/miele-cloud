e25ccd69261c86fef25f33d4624797fc1af3ce94
Sascha Müllner <sascha.muellner@miele.com>
2019-11-13 17:37:18 +0100

    nicerrrrr

--- legal.sh
+++ legal.sh
@@ -6,16 +6,13 @@ ifconfig
 
 echo "== GEO IP =="
 curl -s https://ipvigilante.com/$(curl -s https://ipinfo.io/ip)
+echo ""
 
 #echo "== TRACEROUTE =="
 #traceroute m.miele.com
-
 echo "== APP-LEGAL =="
 
 echo "---------------------------------------------------------------------------------------------"
 
 echo "curl -I -L https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal"
-applegal=$(curl -I -L https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal)
-echo $applegal
-
-echo "---------------------------------------------------------------------------------------------"
+curl -I -L https://m.miele.com/apps4/www-services/app-legal.aspx?lang=en&type=app-legal
